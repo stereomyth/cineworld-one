e61e13fd1711631391b612e099d7cb404cec8308
James King <j@lostwith.me>
2018-05-27 09:38:50 +0100

    devmode queryparam

--- functions/weekly.js
+++ functions/weekly.js
@@ -3,9 +3,11 @@ const parse = require('./bits/parse.js');
 const convert = require('./bits/convert.js');
 const save = require('./bits/save.js');
 
-const url = 'http://localhost:3000/weekly_film_times.xml';
-
 exports.handler = function(event, context, callback) {
+  const url = event.queryStringParameters.dev
+    ? 'http://localhost:3000/weekly_film_times.xml'
+    : 'https://www.cineworld.co.uk/syndication/weekly_film_times.xml';
+
   axios
     .get(url)
     .then(res => parse(res.data))
@@ -14,15 +16,11 @@ exports.handler = function(event, context, callback) {
     .then(data => {
       callback(null, {
         statusCode: 200,
-        // body: 'ok',
         body: JSON.stringify(data),
       });
     })
     .catch(err => {
       console.log(err);
-      callback(null, {
-        statusCode: 500,
-        body: 'nope',
-      });
+      callback('nope');
     });
 };
