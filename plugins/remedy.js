96b9d70580a794f5fae30c766a24bbf1afb61ae6
James King <j@lostwith.me>
2018-05-28 12:40:42 +0100

    remedy axios instance

--- plugins/remedy.js
+++ plugins/remedy.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
+const firebase = 'https://firestore.googleapis.com/v1beta1/';
+
+const remote = axios.create({
+  baseURL: firebase + 'projects/splice-cw/databases/(default)/documents/',
+  params: { pageSize: 200 },
+});
+
 const convert = data => {
   return Object.keys(data).reduce((acc, key) => {
     if (key === 'fields' || key === 'mapValue' || key === 'arrayValue') {
@@ -16,9 +23,5 @@ const convert = data => {
   }, {});
 };
 
-const db = `https://firestore.googleapis.com/v1beta1/projects/splice-cw/databases/(default)/documents/`;
-
 export default (collection, doc = '') =>
-  axios
-    .get(db + collection + '/' + doc, { params: { pageSize: 200 } })
-    .then(res => convert(res.data));
+  remote.get(`${collection}/${doc}`).then(res => convert(res.data));
