a125d404e0e9bcfa609cc80446c77c0befe00621
James King <j@lostwith.me>
2018-05-28 12:31:23 +0100

    remedy location

--- pages/_location.vue
+++ pages/_location.vue
@@ -1,48 +1,39 @@
 <script>
-export default {
-  methods: {
-    log(message) {
-      console.log(message);
-    },
-  },
+import remedy from '~/plugins/remedy';
 
-  computed: {
-    location() {
-      return this.$store.state.weekly[this.$route.params.location];
-    },
+export default {
+  asyncData({ params }) {
+    return remedy('weekly', params.location).then(data => {
+      const { films, ...cinema } = data;
+      return { films, cinema };
+    });
   },
 };
 </script>
 
 <template>
   <section class="container">
-    <!-- <h1>{{location.name}}</h1> -->
-    <pre>location: {{location}}</pre>
-    <!-- <h2>Films</h2>
+    <h1>{{cinema.name}}</h1>
+    <h2>Films</h2>
     <div class="flex">
-      <div v-for="film in films" :key="film.id" class="film" @click="log('hello')">
-        {{ film.title }}
+      <div v-for="film in films" :key="film.id" class="film">
+        <img :src="film.img" alt="">
+        <div>{{ film.title }}</div>
+        <div>{{ film.length }}m</div>
       </div>
-    </div> -->
+    </div>
   </section>
 </template>
 
-<style>
-div {
-  box-sizing: border-box;
-}
-.cinema {
-  padding: 3px;
-  /* margin: 3px; */
-  width: 25%;
-  font-size: 12px;
-}
-
+<style lang="scss">
 .film {
   padding: 3px;
-  /* margin: 3px; */
-  width: 33.3%;
+  width: 20%;
   font-size: 12px;
+
+  img {
+    width: 100px;
+  }
 }
 
 .flex {
