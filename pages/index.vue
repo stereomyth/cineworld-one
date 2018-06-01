f4cd0f18530ea210037137ecf12c3bfcc18df256
James King <j@lostwith.me>
2018-06-01 08:54:09 +0100

    index styles

--- pages/index.vue
+++ pages/index.vue
@@ -11,25 +11,41 @@ export default {
 <template>
   <section class="container">
     <h2>Locations</h2>
-     <div class="flex">
-      <nuxt-link :to="location.slug" class="cinema"
+     <div class="row">
+      <input type="text" placeholder="search" class="search">
+      <nuxt-link :to="location.slug" class="cinema col-sm-6"
         v-for="location in locations" :key="location.slug">
-        {{ location.name }}
+        <div class="name">{{ location.name }}</div>
+        <!-- <div class="postcode">{{ location.postcode }}</div> -->
       </nuxt-link>
     </div>
   </section>
 </template>
 
-<style>
+<style lang="scss">
+.search {
+  width: 100%;
+  background-color: #eee;
+  border: none;
+  padding: 10px 0;
+  padding-left: 15px;
+}
 .cinema {
-  padding: 3px;
-  width: 25%;
-  font-size: 12px;
-  display: block;
+  //   padding: 3px;
+  //   width: 25%;
+  //   font-size: 12px;
+  //   display: block;
+  color: black;
+  margin-bottom: 10px;
 }
 
-.flex {
-  display: flex;
-  flex-wrap: wrap;
+.name {
+  font-size: 16px;
+  font-weight: 100;
+}
+
+.postcode {
+  font-size: 12px;
+  font-weight: 600;
 }
 </style>
