ba8514fd225e35ce52ddde37816c80a488d09ec1
James King <j@lostwith.me>
2018-06-01 08:54:24 +0100

    location styles

--- pages/_location.vue
+++ pages/_location.vue
@@ -16,26 +16,12 @@ export default {
 <template>
   <section class="container">
     <h1>{{cinema.name}}</h1>
-    <h2>Films</h2>
-    <div class="flex">
-      <Film v-for="film in films" :key="film.id" :film="film" />
+    <div class="row">
+      <Film v-for="film in films" :key="film.id" :film="film"
+        class="col-md-6 col-lg-4 col-xl-3" />
     </div>
   </section>
 </template>
 
 <style lang="scss">
-.film {
-  padding: 3px;
-  width: 50%;
-  font-size: 12px;
-
-  img {
-    width: 100px;
-  }
-}
-
-.flex {
-  display: flex;
-  flex-wrap: wrap;
-}
 </style>
