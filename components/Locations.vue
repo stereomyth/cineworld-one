606d2b7e23bc8b0fa73c5a05998f861c5d83d53a
James King <j@lostwith.me>
2018-06-03 15:08:43 +0100

    location keyboard controlls

--- components/Locations.vue
+++ components/Locations.vue
@@ -1,10 +1,26 @@
 <script>
+import { clamp } from 'lodash';
+
 export default {
   props: ['list'],
+
+  created: function() {
+    if (process.client) {
+      document.addEventListener('keydown', this.globalKeys);
+    }
+  },
+
+  destroyed: function() {
+    if (process.client) {
+      document.removeEventListener('keydown', this.globalKeys);
+    }
+  },
 
   data() {
     return {
       term: '',
+      selected: 0,
+      keyb: false,
     };
   },
 
@@ -16,13 +32,44 @@ export default {
     },
   },
 
+  watch: {
+    locations() {
+      this.selected = this.clamp(this.selected);
+    },
+  },
+
   methods: {
     choose(location) {
+      location = location || this.locations[this.selected];
       this.$store.commit('setLocation', location);
       this.$router.push(location.slug);
     },
+    clamp(val) {
+      return clamp(val, 0, this.locations.length - 1);
+    },
     clear() {
       this.term = '';
+    },
+    globalKeys(event) {
+      if (event.keyCode === 40) {
+        event.preventDefault();
+        this.navigate(+1);
+      } else if (event.keyCode === 38) {
+        event.preventDefault();
+        this.navigate(-1);
+      } else if (event.keyCode === 13) {
+        event.preventDefault();
+        this.choose();
+      } else {
+        this.$refs.search.focus();
+      }
+    },
+    navigate(dir) {
+      if (!this.keyb) {
+        this.keyb = true;
+      } else {
+        this.selected = this.clamp(this.selected + dir);
+      }
     },
   },
 };
@@ -33,11 +80,13 @@ export default {
 
     <h3>Select location</h3>
 
-    <input type="text" placeholder="search" class="search" v-model="term" @keyup.esc="clear">
+    <input type="text" placeholder="search" class="search" v-model="term"
+      @keyup.esc="clear" ref="search">
 
     <transition-group name="list" class="row list">
       <div class="cinema col-sm-6" @click="choose(location)"
-        v-for="location in locations" :key="location.slug">
+        v-for="(location, index) in locations" :key="location.slug"
+        :class="{selected: index === selected && keyb}">
         <div class="name">{{ location.name }}</div>
         <!-- <div class="postcode">{{ location.postcode }}</div> -->
       </div>
@@ -57,8 +106,13 @@ export default {
 }
 .cinema {
   color: black;
-  margin-bottom: 5px;
-  margin-top: 5px;
+  padding-bottom: 5px;
+  padding-top: 5px;
+
+  &.selected {
+    background-color: red;
+    color: white;
+  }
 }
 
 .name {
