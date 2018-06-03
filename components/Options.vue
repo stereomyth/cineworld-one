22557368617eee23c66cb810abe92b08a7e827f2
James King <j@lostwith.me>
2018-06-03 15:09:57 +0100

    option

--- components/Options.vue
+++ components/Options.vue
@@ -1,12 +1,29 @@
 <script>
-export default {};
+export default {
+  computed: {
+    location() {
+      return this.$store.state.opts.location;
+    },
+  },
+};
 </script>
 
 <template>
-  <div class="">
-    <h3>OPTIONS</h3>
+  <div class="container">
+    <h3>Options</h3>
+    <div>
+      <div class="label">Location</div>
+      <div class="value">{{location.name}}</div>
+    </div>
+    <hr>
+
   </div>
 </template>
 
 <style lang="scss">
+.label {
+  text-transform: uppercase;
+  font-size: 12px;
+  letter-spacing: 0.1em;
+}
 </style>
